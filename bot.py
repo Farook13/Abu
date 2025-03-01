14be2520e6cdd95778b9bc768119b6c327682de9
Farook13 <umarulfarook787@gmail.com>
2025-03-02 00:15:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+
 import sys
 import glob
 import importlib
@@ -81,6 +82,7 @@ async def Lazy_start():
     today = date.today()
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
+    await LazyPrincessBot.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
