24ad6a344a59e3f7e42ff7777dd3174ea3423ecd
Farook13 <umarulfarook787@gmail.com>
2025-03-02 05:48:18 +0530

    Update render_template.py

--- util/render_template.py
+++ util/render_template.py
@@ -9,31 +9,3 @@ from server.exceptions import InvalidHash
 import urllib.parse
 import logging
 import aiohttp
-
-
-    src = urllib.parse.urljoin(
-        URL,
-        f"{id}/{urllib.parse.quote_plus(file_data.file_name)}?hash={secure_hash}",
-    )
-
-    tag = file_data.mime_type.split("/")[0].strip()
-    file_size = humanbytes(file_data.file_size)
-    if tag in ["video", "audio"]:
-        template_file = "template/req.html"
-    else:
-        template_file = "template/dl.html"
-        async with aiohttp.ClientSession() as s:
-            async with s.get(src) as u:
-                file_size = humanbytes(int(u.headers.get("Content-Length")))
-
-    with open(template_file) as f:
-        template = jinja2.Template(f.read())
-
-    file_name = file_data.file_name.replace("_", " ")
-
-    return template.render(
-        file_name=file_name,
-        file_url=src,
-        file_size=file_size,
-        file_unique_id=file_data.unique_id,
-    )
